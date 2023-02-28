1f3bad61eb00ba124fed834d9c3d810bdc02281a
smallcat <1261415218@qq.com>
2023-03-01 01:17:12 +0800

    商品规格

--- src/api/goods_sku.ts
+++ src/api/goods_sku.ts
@@ -1,9 +1,10 @@
 import api from "./service";
 import qs from "qs";
+import { edit_type } from "@/types/goods_sku";
 
 export const goods_sku_delete = (id: string) => {
   return api.delete(`/goods/goodsSku/delete/${id}`);
 };
 export const goods_sku_list = (id: string) => {
-  return api.get(`/goods/goodsSku/selectByGoodsId?goodsId=${id}`);
+  return api.get<edit_type[]>(`/goods/goodsSku/selectByGoodsId?goodsId=${id}`);
 };
--- src/pages/Goods/components/SetSpec.vue
+++ src/pages/Goods/components/SetSpec.vue
@@ -2,12 +2,21 @@
 //  for node_modules api
 
 import { Setup, Define } from "vue-class-setup";
-import { message } from "ant-design-vue";
+import { message, TableColumnType } from "ant-design-vue";
 import { CloseOutlined } from "@ant-design/icons-vue";
 //  for you api
 import { goods_spec_list } from "@/api/goods_spec";
 
 import type { edit_type } from "@/types/goods_spec";
+
+import { goods_sku_list } from "@/api/goods_sku";
+
+import { edit_type as editType } from "@/types/goods_sku";
+
+interface skuSelectModel {
+  key: string;
+  value: number;
+}
 //  for you components
 
 @Setup
@@ -15,11 +24,14 @@ class SetSpec extends Define<Props, Emits> {
   visible = false;
   id: string = "";
   specList: edit_type[] = [];
+  goodsSkuList: editType[] = [];
+  skuSelect: skuSelectModel[] = [];
 
   toggleShow(id: string) {
     this.visible = true;
     this.id = id;
     this.getGoodsSpecList(id);
+    this.getGoodsSkuList(id);
   }
 
   delTag(index: number, key: number) {
@@ -38,6 +50,33 @@ class SetSpec extends Define<Props, Emits> {
       message.error(res.message);
     }
   }
+
+  async getGoodsSkuList(id: string) {
+    const res = await goods_sku_list(id);
+    if (res && res.code == 200) {
+      this.goodsSkuList = res.data;
+    } else {
+      message.error(res.message);
+    }
+  }
+
+  columns: TableColumnType[] = [
+    {
+      title: "规格名称",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "规格",
+      dataIndex: "spec",
+      key: "spec",
+    },
+    {
+      title: "价格",
+      dataIndex: "price",
+      key: "price",
+    },
+  ];
 }
 </script>
 
@@ -64,12 +103,35 @@ defineExpose({
     v-model:visible="ssku.visible"
     title="规格"
     @ok="ssku.handleOk"
+    width="1000px"
   >
     <a-space style="margin-bottom: 12px">
+      <!-- <a-input v-model:value="ssku.name" placeholder="请填写SKU名称！" />
+      <a-button type="primary" @click="ssku.setSku()">添加</a-button> -->
+      <a-form-item
+        v-for="(item: editType, index: number) in ssku.goodsSkuList"
+        :key="item.id"
+        :label="item.name"
+        name="username"
+        :rules="[{ required: true, message: 'Please input your username!' }]"
+      >
+        <a-select
+          ref="select"
+          style="width: 120px"
+          v-model="ssku.skuSelect[index].key"
+        >
+          <a-select-option
+            v-for="child in item.goodsSkuValueModelList"
+            :key="child.id"
+            :value="child.id"
+            >{{ child.value }}</a-select-option
+          >
+        </a-select>
+      </a-form-item>
       <a-input v-model:value="ssku.name" placeholder="请填写SKU名称！" />
       <a-button type="primary" @click="ssku.setSku()">添加</a-button>
     </a-space>
-    <a-collapse v-model:activeKey="ssku.activeKey" accordion>
+    <!-- <a-collapse v-model:activeKey="ssku.activeKey" accordion>
       <a-collapse-panel
         v-for="(item, index) in ssku.goodsSkuModelList"
         :key="index"
@@ -89,7 +151,8 @@ defineExpose({
           </template>
         </a-tag>
       </a-collapse-panel>
-    </a-collapse>
+    </a-collapse> -->
+    <a-table :dataSource="ssku.specList" :columns="ssku.columns"></a-table>
   </a-modal>
 </template>
 
